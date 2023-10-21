e5df26ccc3eb012b274a1f5f3804a716b12e7395
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-10-20 23:29:58 -0300

    feat: using usacase in grpc service

--- grpc-server/grpc/services/product.go
+++ grpc-server/grpc/services/product.go
@@ -4,30 +4,57 @@ import (
 	"context"
 
 	"github.com/full-cycle-challenge/grpc/pb"
+	"github.com/full-cycle-challenge/usecase"
 )
 
 type ProductServiceGrpc struct {
+	ProductUseCase usecase.ProductUseCase
 	pb.UnimplementedProductServiceServer
 }
 
 func (p *ProductServiceGrpc) CreateProduct(ctx context.Context, input *pb.CreateProductRequest) (*pb.CreateProductResponse, error) {
+	response, err := p.ProductUseCase.Create(
+		input.Name,
+		input.Description,
+		input.Price,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
 	return &pb.CreateProductResponse{
 		Product: &pb.Product{
-			Id:          1,
-			Name:        input.Name,
-			Description: input.Description,
-			Price:       input.Price,
+			Id:          response.ID,
+			Name:        response.Name,
+			Description: response.Description,
+			Price:       response.Price,
 		},
 	}, nil
 }
 
 func (p *ProductServiceGrpc) FindProducts(ctx context.Context, input *pb.FindProductsRequest) (*pb.FindProductsResponse, error) {
-	list := make([]*pb.Product, 1)
+	list, err := p.ProductUseCase.FindAll()
+	if err != nil {
+		return nil, err
+	}
+
+	response := make([]*pb.Product, len(list))
+	for i := range list {
+		response[i] = &pb.Product{
+			Id:          list[i].ID,
+			Name:        list[i].Name,
+			Description: list[i].Description,
+			Price:       list[i].Price,
+		}
+	}
 	return &pb.FindProductsResponse{
-		Products: list,
+		Products: response,
 	}, nil
 }
 
-func NewProductServiceGrpc() *ProductServiceGrpc {
-	return &ProductServiceGrpc{}
+func NewProductServiceGrpc(productUseCase usecase.ProductUseCase) *ProductServiceGrpc {
+	return &ProductServiceGrpc{
+		ProductUseCase: productUseCase,
+	}
 }
